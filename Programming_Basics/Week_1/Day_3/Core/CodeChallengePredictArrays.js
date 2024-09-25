7b1e56748762e31f4d18d0eaf6107495379fcc2e
farouk ghezal <faroukghezal3@gmail.com>
2024-09-25 08:22:07 +0100

    Code Challenge: Predict Arrays

---
+++ Programming_Basics/Week_1/Day_3/Core/CodeChallengePredictArrays.js
@@ -0,0 +1,20 @@
+var contactInfo = ["Paula", "Smith", "1234 Main Street", "St. Louis", "MO", 12345];
+console.log(contactInfo);
+
+
+var produce = ["apples", "oranges"];
+var frozen = ["broccoli", "ice cream"];
+frozen.push("hashbrowns");
+console.log(frozen);
+
+var movieLibrary = ["Bambi", "E.T.", "Toy Story"];
+movieLibrary.push("Zorro");
+movieLibrary[1] = "Beetlejuice";
+console.log(movieLibrary);
+
+
+/*T diagram : 
+contactInfo = ["Paula", "Smith", "1234 Main Street", "St. Louis", "MO", 12345]
+frozen = ["broccoli", "ice cream", "hashbrowns"]
+movieLibrary = ["Bambi", "Beetlejuice", "Toy Story", "Zorro"]
+*/
